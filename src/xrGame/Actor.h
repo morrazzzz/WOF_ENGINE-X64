be324e5fd2abd705977d1be4e7a7da31af62f89c
Maxivanov2002 <65188965+Maxivanov2002@users.noreply.github.com>
2023-07-18 07:35:40 +0300

    UPD Плавные приседания ГГ

--- src/xrGame/Actor.h
+++ src/xrGame/Actor.h
@@ -447,6 +447,7 @@ public:
 	bool					CanJump					();
 	bool					CanMove					();
 	float					CameraHeight			();
+	float					CurrentHeight; 	// Alex ADD: for smooth crouch fix
 	bool					CanSprint				();
 	bool					CanRun					();
 	void					StopAnyMove				();
