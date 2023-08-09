0c17344194d1e079613617a829bdb032405c13df
Maxivanov2002 <65188965+Maxivanov2002@users.noreply.github.com>
2023-08-09 12:25:36 +0300

    UPD Обход для недопустимых игровых вершин Спасибо @ggGhosTt  Разделил под x64 и x86

--- src/xrGame/game_graph_inline.h
+++ src/xrGame/game_graph_inline.h
@@ -111,9 +111,14 @@ IC	const float &CGameGraph::edge_weight							(const_iterator i) const
 	return						(i->distance());
 }
 
-IC	const CGameGraph::CVertex *CGameGraph::vertex					(u32 const vertex_id) const
+IC	const CGameGraph::CVertex* CGameGraph::vertex(u32 const vertex_id) const
 {
+#ifdef _M_X64
+	return (valid_vertex_id(vertex_id)) ? (m_nodes + vertex_id)
+		: (m_nodes + header().vertex_count() - 1);
+#else
 	return						(m_nodes + vertex_id);
+#endif
 }
 
 IC	const u8 &CGameGraph::CHeader::version							() const
