1aaa4028e480729e1209af7f0dbf436ff339892f
Maxivanov2002 <65188965+Maxivanov2002@users.noreply.github.com>
2023-09-13 11:00:50 +0300

    Revert "Fix rain sound after loading or changing level"
    This reverts commit 7194b059732496b13e21b56ad866b382e1989be8.

--- src/xrEngine/Rain.cpp
+++ src/xrEngine/Rain.cpp
@@ -155,11 +155,7 @@ void	CEffect_Rain::OnFrame	()
 	switch (state)
 	{
 	case stIdle:		
-		if (factor < EPS_L) {
-			if (snd_Ambient._feedback())
-				snd_Ambient.stop();
-			return;
-		}
+		if (factor<EPS_L)		return;
 		state					= stWorking;
 		snd_Ambient.play		(0,sm_Looped);
 		snd_Ambient.set_position(Fvector().set(0,0,0));
